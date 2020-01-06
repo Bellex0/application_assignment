7a0a15360a38ec13236b95765d26377a11dc2204
bellex0 <bellepx3@gmail.com>
2020-01-06 05:05:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var quotes = require('random-movie-quotes')
 
 app.use(express.static('public'));
 
-app.get('/quote', (req, res) => {
-  res.json({
-    quote: quotes.getQuote()
-  });
-});
+app.route('/quote')
+  .get(function(req, res){
+    res.json({
+      quote: quotes.getQuote()
+    });
+  })
+
 
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
